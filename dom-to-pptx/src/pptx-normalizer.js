4ffde4a46b1e767f25aa7c7a0dd0ee60bfa6b92e
JC <576461713@qq.com>
2026-05-29 16:29:11 +0800

    Sync dom-to-pptx support modules

--- dom-to-pptx/src/pptx-normalizer.js
+++
@@ -1,9 +0,0 @@
-export async function normalizePptxZip(zip) {
-  const contentTypes = zip.file('[Content_Types].xml');
-  if (!contentTypes) return zip;
-
-  const xml = await contentTypes.async('string');
-  const normalized = xml.replace(/<Override[^>]+PartName="\/ppt\/fonts\/[^"]+"[^>]*\/>/g, '');
-  zip.file('[Content_Types].xml', normalized);
-  return zip;
-}
